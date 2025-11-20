b9a464139df6ba3c055877f2a985fb8757287d3a
Martin Høst Normark <martin.hoest.normark@lego.com>
2025-11-20 13:58:02 +0100

    Export additional core DDD components: ValueObject, DomainError, Result, and Guard

--- packages/core/index.ts
+++ packages/core/index.ts
@@ -1,2 +1,6 @@
 export * from './src/entity';
 export * from './src/aggregate-root';
+export * from './src/value-object';
+export * from './src/errors';
+export * from './src/result';
+export * from './src/guard';
